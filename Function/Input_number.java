7555d9f6ab247fba9f9f78d2bf2680e094676e5c
tushar23 <tusharpandey070@gmail.com>
2022-11-03 15:50:53 +0530

    java_dsa

--- Function/Input_number.java
+++ Function/Input_number.java
@@ -24,4 +24,19 @@ public class Input_number {
             System.out.println("sum= "+s);
         }
 
+    public static class Swap {
+        public static void main(String[] args) {
+
+            int a=10;
+            int b=20;
+    //   swap number code
+            int temp;
+            temp=a;
+            a=b;
+            b=temp;
+
+            System.out.println(a + "  "+ b);
+
+        }
+    }
 }
